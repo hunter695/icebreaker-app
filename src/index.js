a6aa6ab924f027dd4c4ad06d4457f2003b9ee0e8
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-03-06 01:06:58 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
+
+const rootEl = document.getElementById('root')
+const render = (Component) =>
+  // eslint-disable-next-line react/no-render-return-value
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    rootEl
+  )
+
+render(App)
+if (module.hot) module.hot.accept(() => render(App))
