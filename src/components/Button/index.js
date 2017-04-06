21a7b11ca67a4146246ee6dbbdc51d3d7ba76c47
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-04-06 05:33:50 -0400

    Modify button styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 const button = styled.button`
   background-color: #b7d993;
   border: none;
-  padding: 16px;
+  max-height: 60px;
+  padding: 0 16px;
   &:focus {
     outline: none;
   }
