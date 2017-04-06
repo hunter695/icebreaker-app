8e731ac0da24162ec25f0e796b018279df65f6bc
Igor Goldvekht <goldvekht.igor@gmail.com>
2017-04-06 05:35:11 -0400

    Implement a Switcher component

---
+++ src/scenes/Contribute/components/Switcher/index.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react'
+import styled from 'styled-components'
+import Toggle from 'components/Toggle'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  :nth-child(2) {
+    margin: 0 8px;
+  }
+`
+
+const Highlight = styled.span`
+  color: #fff;
+`
+
+const Dim = styled.span`
+  color: rgba(44, 44, 44, 0.3);
+`
+
+const Switcher = ({ checked, choices, name, onChange }) => (
+  <Wrapper>
+    {checked ? <Dim>{choices[0]}</Dim> : <Highlight>{choices[0]}</Highlight> }
+    <Toggle name={name} onChange={onChange} />
+    {checked ? <Highlight>{choices[1]}</Highlight> : <Dim>{choices[1]}</Dim> }
+  </Wrapper>
+)
+
+Switcher.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  choices: PropTypes.arrayOf(PropTypes.string).isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default Switcher
