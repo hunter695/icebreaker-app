a888c16d7c998c1dd18eedf44f201ddc0d1648af
Nathan <dobobobado@gmail.com>
2017-05-08 11:34:54 -0400

    update Button CSS style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
 const button = styled.button`
-  background-color: #b7d993;
+  background-color: #ffc33a;
   border: none;
   max-height: 60px;
   padding: 0 16px;
+  border-radius: 7px;
+  font-family: "Arial", "Helvetica", sans-serif;
+  font-size: 21px;
+  font-weight: bold;
+  color: white;
   &:focus {
     outline: none;
   }
